8d8e2ebce80ea2dba5b1a8a7b77a78eed5df347c
Mohammad Motiur Rahman <mdmotiurrahman4u@gmail.com>
2026-02-08 17:55:33 +0530

    container config code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "basicsjs@google.com"
+var accountPassword = "12345"
+accountCity = "bihar"
+
+// accountId = 2  // not allowed
+accountEmail = "abc@.com"
+accountPassword = 123456
+accountCity = "kolkata"
+let accountState ;
+ 
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
