056e0473b28ca396f211ea930b1d23963befd9ed
Mohammad Motiur Rahman <mdmotiurrahman4u@gmail.com>
2026-02-08 21:33:03 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"Use strict"; // treat all js code as newer version
+
+// alert(3 + 5) //we are using nodejs, not browser
+
+let name = "Motiur";
+let age = 21;
+let isLoggedIn = true;
+
+// Number => 2 to power 53
+// Bigint
+// string     =>""
+// booleanm   => true/false
+// null       => standalone value
+// undefined  => let state;
+// symbol     => use in react (unique)
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+console.log(typeof "Motiur");
+console.log(typeof age);
