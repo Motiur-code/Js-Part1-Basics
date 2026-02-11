24c65a3e57ca1ee47df22403c5948fa9c27ef30b
Mohammad Motiur Rahman <mdmotiurrahman4u@gmail.com>
2026-02-11 22:49:46 +0530

    switch case

---
+++ 04_logic_flow/switch.js
@@ -0,0 +1,55 @@
+/*
+switch (key) {
+  case value:
+    
+    break;
+
+  default:
+    break;
+}
+    */
+
+//if we not write break then all code execute accept  **default**
+
+/*
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("february");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
+    */
+
+const monthNew = "march";
+
+switch (monthNew) {
+  case "jan":
+    console.log("January");
+    break;
+  case "feb":
+    console.log("february");
+    break;
+  case "march":
+    console.log("march");
+    break;
+  case "april":
+    console.log("april");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
